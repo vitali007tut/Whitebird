7c208b20e34f632ccb3831bbc504b3898e43d2ed
Vitali <vitali007@tut.by>
2024-11-16 16:04:02 +0300

    feat: add create/remove post and pagination

--- src/components/Posts/index.tsx
+++ src/components/Posts/index.tsx
@@ -1,9 +1,17 @@
-import { Button, Box } from "@mui/material";
+import {
+  Button,
+  Box,
+  Paper,
+  TextField,
+  Pagination,
+  PaginationItem,
+  Typography,
+} from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import * as sx from "./styles";
 import { useNavigate } from "react-router-dom";
 import { getFavotiteArray } from "../utils";
-import { log } from "console";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 type TPosts = {
   id: string;
@@ -19,9 +27,13 @@ type TProps = {
 export const Posts = ({ userId }: TProps) => {
   const [posts, setPosts] = useState<TPosts[]>([]);
   const [loading, setLoading] = useState(true);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
   const navigate = useNavigate();
-  const ref = useRef<TPosts[]>()
-  const refFilterMode = useRef<boolean>(false)
+  const ref = useRef<TPosts[]>();
+  const refFilterMode = useRef<boolean>(false);
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 10;
 
   useEffect(() => {
     const url = userId
@@ -33,30 +45,74 @@ export const Posts = ({ userId }: TProps) => {
       .then((json) => {
         if (refFilterMode.current) {
           ref.current = json;
-          const array = getFavotiteArray()
-          const filtered = ref.current?.filter(post => array.includes(Number(post.id)))
-          setPosts(filtered??[])
-        } else setPosts(json)
+          const array = getFavotiteArray();
+          const filtered = ref.current?.filter((post) =>
+            array.includes(Number(post.id))
+          );
+          setPosts(filtered ?? []);
+        } else setPosts(json);
       })
       .finally(() => setLoading(false));
+
+      setPage(1)
   }, [userId]);
 
   const handleClickPost = (id: string) => {
-    navigate(`/post/${id}`)
-  }
+    navigate(`/post/${id}`);
+  };
 
   const showFavorite = () => {
     ref.current = posts;
-    const array = getFavotiteArray()
-    const filtered = posts.filter(post => array.includes(Number(post.id)))
-    setPosts(filtered)
-    refFilterMode.current = true
-  }
+    const array = getFavotiteArray();
+    const filtered = posts.filter((post) => array.includes(Number(post.id)));
+    setPosts(filtered);
+    refFilterMode.current = true;
+  };
 
   const showAll = () => {
-    setPosts(ref.current??[])
-    refFilterMode.current = false
-  }
+    setPosts(ref.current ?? []);
+    refFilterMode.current = false;
+  };
+
+  const handleDeletePost = (postId: number) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+      method: "DELETE",
+    });
+    setPosts((prev) => prev.filter((item) => Number(item.id) !== postId));
+  };
+
+  const addPost = () => {
+    const numberId = userId ? userId : Math.floor(Math.random() * 10 + 1);
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        body,
+        userId: numberId,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        json.id += Math.floor(Math.random() * 100);
+        setPosts(posts.concat(json));
+      });
+    setTitle("");
+    setBody("");
+  };
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
+  const currentPageItems = posts.slice(
+    (page - 1) * itemsPerPage,
+    page * itemsPerPage
+  );
 
   return (
     <div>
@@ -68,26 +124,77 @@ export const Posts = ({ userId }: TProps) => {
             Found {posts.length} posts by userId: {userId}
           </div>
         )}
-        {!refFilterMode.current && <Button size="small" variant="contained" onClick={showFavorite}>
-          Show favorite posts</Button>}
-        {refFilterMode.current && <Button 
-        size="small" 
-        variant="contained"
-        color="secondary"
-        onClick={showAll}
-        >Сancel favorite filtration</Button>}
+        {!refFilterMode.current && (
+          <Button size="small" variant="contained" onClick={showFavorite}>
+            Show favorite posts
+          </Button>
+        )}
+        {refFilterMode.current && (
+          <Button
+            size="small"
+            variant="contained"
+            color="secondary"
+            onClick={showAll}
+          >
+            Сancel favorite filtration
+          </Button>
+        )}
       </Box>
       <ul>
-        {posts.length !== 0 &&
-          posts.map((post) => (
-            <li key={post.id}>
-              <Box sx={sx.card}>
+        {currentPageItems.map((post) => (
+          <li key={post.id}>
+            <Box sx={sx.card}>
+              <div>
                 <span>{post.title}</span>
-                <Button size="small" onClick={() => handleClickPost(post.id)}>Learn More</Button>
-              </Box>
-            </li>
-          ))}
+                <Button size="small" onClick={() => handleClickPost(post.id)}>
+                  Learn More
+                </Button>
+              </div>
+              <DeleteIcon
+                style={{ cursor: "pointer" }}
+                onClick={() => handleDeletePost(Number(post.id))}
+              />
+            </Box>
+          </li>
+        ))}
       </ul>
+      <Pagination
+        count={Math.ceil(posts.length / itemsPerPage)}
+        page={page}
+        onChange={handlePageChange}
+        renderItem={(item) => (
+          <PaginationItem
+            {...item}
+
+            sx={sx.paginationItem}
+          />
+        )}
+      />
+
+      <Paper elevation={3} sx={sx.paper}>
+        <Typography>Creating post</Typography>
+        <Box sx={sx.commentsContainer}>
+          <TextField
+            sx={sx.field}
+            label="Title"
+            value={title}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setTitle(event.target.value);
+            }}
+          />
+          <TextField
+            sx={sx.field}
+            label="Body"
+            value={body}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setBody(event.target.value);
+            }}
+          />
+          <Button size="small" variant="contained" onClick={addPost}>
+            Add post
+          </Button>
+        </Box>
+      </Paper>
     </div>
   );
 };
--- src/components/Posts/styles.ts
+++ src/components/Posts/styles.ts
@@ -2,6 +2,7 @@ export const title = {
   height: "30px",
   display: "flex",
   justifyContent: "space-between",
+  marginBottom: "17px",
 };
 
 export const card = {
@@ -9,4 +10,47 @@ export const card = {
   padding: "5px 15px",
   marginBottom: "5px",
   borderRadius: "5px",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
+
+export const paper = {
+  color: "#fff",
+  background: "#1e1e1e",
+  padding: "10px 20px",
+  marginTop: "25px",
+};
+
+export const commentsContainer = {
+  display: "grid",
+  gridTemplateColumns: "1fr 3fr 1fr",
+  gridColumnGap: "10px",
+  marginTop: "10px",
+};
+
+export const field = {
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "#fff",
+    },
+    "&:hover fieldset": { borderColor: "#1976b4" },
+    "& input": { color: "#fff" },
+  },
+  "& .MuiInputLabel-root": { color: "#fff" },
+  "& .MuiInputLabel-root.Mui-focused": { color: "#1976b4" },
+};
+
+export const paginationItem = {
+  "&.MuiPaginationItem-root": {
+    color: "#fff",
+  },
+  "&.MuiPaginationItem-page": {
+    color: "#fff",
+  },
+  "&.Mui-selected": { backgroundColor: "#1976d2", color: "#fff" },
+  "&.MuiPaginationItem-page:hover": {
+    backgroundColor: "lightblue",
+    color: "#000",
+  },
 };
